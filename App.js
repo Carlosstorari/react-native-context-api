8aec303c86dfc21750706e8285820b6a84089c0a
carlosStorari <carlos.storari95@gmail.com>
2024-03-23 10:56:45 -0300

    use context api pra mudanca tema

--- App.js
+++ App.js
@@ -1,10 +1,13 @@
 import Rotas from "./src/rotas";
-import { InfoProvider } from "./src/contexts/GlobalContext";
+import { TemaProvider } from "./src/contexts/TemaContext";
+import { AutenticacaoProvider } from "./src/contexts/AutenticacaoContext";
 
 export default function App() {
   return (
-    <InfoProvider>
-      <Rotas />
-    </InfoProvider>
+    <TemaProvider>
+      <AutenticacaoProvider>
+        <Rotas />
+      </AutenticacaoProvider>
+    </TemaProvider>
   );
 }
